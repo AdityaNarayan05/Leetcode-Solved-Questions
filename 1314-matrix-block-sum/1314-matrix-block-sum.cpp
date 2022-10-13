ac9ed71d1da3d1c96a67a84f88c8bed991a6ebbf
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-14 00:01:04 +0530

    Time: 11 ms (88.38%), Space: 9.6 MB (10.69%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
-    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        vector<vector<int>> ans(mat.size(),vector<int>(mat[0].size(),0));
-        for(int i=0;i<mat.size();i++)
-            for(int j=0;j<mat[i].size();j++)
-                ans[i][j]=sum(mat,i,j,k);
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int K) {
+        vector<vector<int>> prefix_sum = prefixSum(mat);
+        vector<vector<int>> answer = prefix_sum;
         
-        return ans;
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[0].size(); j++) {
+                int upper_i = ((i+K) >= mat.size()) ? (mat.size()-1) : (i+K);
+                int upper_j = ((j+K) >= mat[0].size()) ? (mat[0].size()-1) : (j+K);
+                
+                int lower_i = ((i-K) <= 0 ) ? 0 : (i-K);
+                int lower_j = ((j-K) <= 0 ) ? 0 : (j-K);
+            
+                
+                answer[i][j] = prefix_sum[upper_i][upper_j] - ((lower_i == 0) ? 0 : prefix_sum[lower_i-1][upper_j]) - ((lower_j == 0) ? 0 : prefix_sum[upper_i][lower_j-1]) + ((lower_i == 0) || (lower_j == 0)  ? 0 : prefix_sum[lower_i-1][lower_j-1]);
+            }
+        }
+    
+        
+        return answer;
+        
     }
     
-    int sum(vector<vector<int>> &mat,int i,int j,int k){
-        long ans=0;
-        int s1=max(0,i-k);
-        int s2=max(0,j-k);
-        int m=mat.size();
-        int n=mat[0].size();
-        int e1=min(m-1,i+k);
-        int e2=min(n-1,j+k);
+    vector<vector<int>> prefixSum(vector<vector<int>>& mat) {
+        vector<vector<int>> prefix_sum = mat;
         
-        for(int p=s1;p<=e1;p++)
-            for(int q=s2;q<=e2;q++)
-                ans+=mat[p][q];
-        
-        return ans;
+        for (int i = 0; i < mat.size(); i++) {
+            int sum = 0;
+            for (int j = 0; j < mat[0].size(); j++) {
+                sum = sum + mat[i][j];
+                if (i > 0) {
+                    prefix_sum[i][j] = sum + prefix_sum[i-1][j];
+                } else {
+                    prefix_sum[i][j] = sum;   
+                }
+            }
+        }
+        return prefix_sum;
     }
 };
