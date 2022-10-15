f187f2a71c74c6838c254c914d18a45d7c0813d3
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-15 14:36:55 +0530

    Time: 16 ms (62.07%), Space: 10 MB (56.69%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        return getMinPath(grid,0,0,dp);
-    }
-    
-    int getMinPath(vector<vector<int>> &grid,int i,int j,vector<vector<int>> &dp){
-        if(i==grid.size()-1 and j==grid[0].size()-1)
-            return grid[i][j];
-        if(j==grid[0].size()-1)
-            return grid[i][j]+getMinPath(grid,i+1,j,dp);
-        if(i==grid.size()-1)
-            return grid[i][j]+getMinPath(grid,i,j+1,dp);
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> sum(m,vector<int>(n,grid[0][0]));
         
-        return dp[i][j]=grid[i][j]+min(getMinPath(grid,i+1,j,dp),getMinPath(grid,i,j+1,dp));
+        for(int i=1;i<m;++i)
+            sum[i][0]=sum[i-1][0]+grid[i][0];
+        
+        for(int j=1;j<n;j++)
+            sum[0][j]=sum[0][j-1]+grid[0][j];
+        
+        for(int i=1;i<m;++i)
+            for(int j=1;j<n;j++)
+                sum[i][j]=min(sum[i-1][j],sum[i][j-1])+grid[i][j];
+        
+        return sum[m-1][n-1];
     }
 };
