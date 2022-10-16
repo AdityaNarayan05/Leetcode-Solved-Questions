e08d6106f5bbd1af77cb800f9e655a9e6378f95f
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-16 23:30:52 +0530

    Time: 130 ms (34.93%), Space: 7.8 MB (59.66%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -4,28 +4,19 @@ public:
         int n=jobDifficulty.size();
         if(d>n)
             return -1;
-        vector<vector<int>> dp(n+1,vector<int>(d+1,-1));
-        return mDiff(jobDifficulty,0,n,d,dp);
-    }
-    
-    int mDiff(vector<int> &jD,int start,int n,int d,vector<vector<int>> &dp){
-        if(d==1){
-            int maxi=jD[start];
-            for(int i=start+1;i<n;i++)
-                maxi=max(maxi,jD[i]);
-            
-            return maxi;
+        vector<vector<int>> dp(n+1,vector<int>(d+1,1e9));
+        dp[0][0] = 0;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= d; j++){
+                int maxi = INT_MIN;
+                for(int k = i - 1; k >= j - 1; k--){
+                    maxi = max(maxi, jobDifficulty[k]);
+                    dp[i][j] = min(dp[i][j], maxi + dp[k][j - 1]);
+                }
+            }
         }
-        if(dp[start][d]!=-1)
-            return dp[start][d];
-        
-        int maxi=INT_MIN;
-        int res=INT_MAX;
-        
-        for(int i=start;i<n-d+1;i++){
-            maxi=max(maxi,jD[i]);
-            res=min(res,maxi+mDiff(jD,i+1,n,d-1,dp));
-        }
-        return dp[start][d]=res;
+
+        return dp[n][d];
     }
 };
