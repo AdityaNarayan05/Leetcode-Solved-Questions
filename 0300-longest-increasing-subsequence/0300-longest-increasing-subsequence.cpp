5db251e06368f3db55a99c175fef29c8854760ee
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-17 23:11:45 +0530

    Time: 1260 ms (17.57%), Space: 10.6 MB (30.59%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    vector<int> dp;
     int lengthOfLIS(vector<int>& nums) {
-        dp.resize(size(nums), vector<int>(1+size(nums), -1));   
+        dp.resize(nums.size()+1,-1);
         return solve(nums, 0, -1);
     }
-    int solve(vector<int>& nums, int i, int prev_i) {
+    int solve(vector<int>& nums, int i, int prev) {
         if(i >= size(nums))
             return 0;
         
-        if(dp[i][prev_i+1] != -1)
-            return dp[i][prev_i+1];
+        if(dp[prev+1] != -1)
+            return dp[prev+1];
         
-        int take = 0, dontTake = solve(nums, i + 1, prev_i);
-        if(prev_i == -1 || nums[i] > nums[prev_i])
+        int take = 0, dontTake = solve(nums, i + 1, prev);
+        if(prev == -1 || nums[i] > nums[prev])
             take = 1 + solve(nums, i + 1, i);
         
-        return dp[i][prev_i+1] = max(take, dontTake);
+        return dp[prev+1] = max(take, dontTake);
     }
 };
