a226633d9e09f2f81a64b6a532b39984bf886bc6
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-19 07:09:42 +0530

    Time: 17 ms (87.27%), Space: 8.5 MB (89.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
-    #define vi vector<int>
-    #define vvi vector<vi>
-    #define rep(i,a,b) for(int i=a;i<b;i++)
-    int longestCommonSubsequence(string s1, string s2) {
-        int n=s1.size();
-    int m=s2.size();
-        vvi d(n+1,vi(m+1,-1));
-    rep(i,0,n+1){
-        rep(j,0,m+1){
-            if(i==0 or j==0){
-                d[i][j]=0;
-                continue;
-            }
-            if(s1[i-1]==s2[j-1]){
-                d[i][j]=1+d[i-1][j-1];
-            }
-            else{
-                d[i][j]=max(d[i][j-1],d[i-1][j]);
-            }
-        }
-    }
-    return d[n][m];
+    int longestCommonSubsequence(string a,string b){
+        short m[1001][1001] = {};
+        for (auto i = 0; i < a.size(); ++i)
+            for (auto j = 0; j < b.size(); ++j)
+                m[i + 1][j + 1] = a[i] == b[j] ? m[i][j] + 1 : max(m[i + 1][j], m[i][j + 1]);
+        
+        return m[a.size()][b.size()];
     }
 };
