591a2b8db1641df74519d15c279f752ae37822fb
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-20 13:20:44 +0530

    Time: 157 ms (82.41%), Space: 9.2 MB (51.13%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    vector<int> dp;
+    
     int numSquares(int n) {
-        dp.resize(n+1,-1);
-        return numSq(n);
-    }
-    int numSq(int n){
-        if(n==0)
-            return 0;
-        if(n<0)
-            return INT_MAX-1;
-        if(dp[n]!=-1)
-            return dp[n];
-        int ans=INT_MAX;
-        for(int i=1;i*i<=n;i++)
-            ans=min(ans,1+numSq(n-i*i));
-        
-        return dp[n]=ans;
+        vector<int> dp(n+1,0);
+        for(int j=1;j<=n;j++){
+            int ans=INT_MAX;
+            for(int i=1;i*i<=j;i++)
+                ans=min(ans,1+dp[j-i*i]);
+            
+            dp[j]=ans;
+        }
+        return dp[n];
     }
 };
