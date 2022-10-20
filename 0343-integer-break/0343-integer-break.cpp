c0fcbf0a6d04325487d3622ad4ccd508ec428636
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-20 13:09:47 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (90.13%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+    int dp[58+1][57+1];
     int integerBreak(int n) {
+        memset(dp,-1,sizeof(dp));
         return Break(n,n-1);
     }
     int Break(int n,int curr){
         if(n==0 or curr==0)
             return 1;
+        if(dp[n][curr]!=-1)
+            return dp[n][curr];
         if(curr>n)
-            return Break(n-0,curr-1);
+            return dp[n][curr]=Break(n-0,curr-1);
         else
-            return max(Break(n-0,curr-1),curr*Break(n-curr,curr));
+            return dp[n][curr]=max(Break(n-0,curr-1),curr*Break(n-curr,curr));
     }
 };
