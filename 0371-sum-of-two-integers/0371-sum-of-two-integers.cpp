8cf8d1a148c1344ad9006e96abb6b1bdb241ba5c
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-25 22:55:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.95%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        if(b==0)
+            return a;
+        int sum_without_carry = a^b;
+        int carry = (unsigned int)(a&b)<<1;
+        return getSum(sum_without_carry, carry);
     }
 };
