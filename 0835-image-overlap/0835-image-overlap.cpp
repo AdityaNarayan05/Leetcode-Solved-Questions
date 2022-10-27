2fd34d3cd859f4397bec6ecf134b8b2513fd74fe
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-27 19:48:32 +0530

    Time: 106 ms (72.92%), Space: 12.2 MB (34.03%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    int overlap(vector<vector<int>>& img1, vector<vector<int>>& img2, int x, int y) {
-        int n = img1.size();
-        int res = 0, startX = 0, startY = 0, stopX = n - abs(x), stopY = n - abs(y);
-        if (x < 0) {
-            startX = abs(x);
-            stopX = n;
-        } 
-        if (y < 0) {
-            startY = abs(y);
-            stopY = n;
-        }
-        for (int i = startX; i < stopX; ++ i) {
-            for (int j = startY; j < stopY; ++ j) 
-                res += (img1[i + x][j + y] + img2[i][j] == 2);
-        }
-        return res;
-    }
-    
-    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        int res = 0, n = img1.size();
-        for (int x = 1 - n; x < n; ++ x)
-            for (int y = 1 - n; y < n; ++ y)
-                res = max(res, overlap(img1, img2, x, y));
+    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<int> LA, LB;
+        int N = A.size();
+        unordered_map<int, int> count;
+        for (int i = 0; i < N * N; ++i)
+            if (A[i / N][i % N] == 1)
+                LA.push_back(i / N * 100 + i % N);
+        for (int i = 0; i < N * N; ++i)
+            if (B[i / N][i % N] == 1)
+                LB.push_back(i / N * 100 + i % N);
+        for (int i : LA) for (int j : LB) count[i - j]++;
+        int res = 0;
+        for (auto it : count) res = max(res, it.second);
         return res;
     }
 };
