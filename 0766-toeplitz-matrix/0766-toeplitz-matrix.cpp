47674979a3bee024fe06bcf4e6c3e4e4d141d49a
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-10-31 10:27:27 +0530

    Time: 36 ms (32.22%), Space: 17.5 MB (63.25%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -4,8 +4,9 @@ public:
         int m = matrix.size(), n = matrix[0].size();
         for (int i = 1; i < m; i++)
             for (int j = 1; j < n; j++)
-                if (matrix[i][j] != matrix[i - 1][j - 1])
-                    return false;
+                if(i - 1 >=0 && i - 1 < m && j - 1 >= 0 && j - 1 < n)
+                    if (matrix[i][j] != matrix[i - 1][j - 1])
+                        return false;
         return true;
     }
 };
