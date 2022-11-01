058bb08cba1e4a718441abc968af7c77551f7ec9
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-01 12:41:33 +0530

    Time: 32 ms (56.44%), Space: 19.7 MB (81.72%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left=0,right=height.size()-1,maxL=0,maxR=0,total=0;
+        while(left<right){
+            if(height[left]<=height[right]){
+                if(height[left]>=maxL){
+                    maxL=height[left];
+                } else{
+                    total+=maxL-height[left];
+                }
+                left++;
+            } else{
+                if(height[right]>=maxR){
+                    maxR=height[right];
+                } else{
+                    total+=maxR-height[right];
+                }
+                right--;
+            }
+        }
+        return total;
+    }
+};
