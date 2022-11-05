5b7be866242baea63b413b9a11bdcc985f6a2ca6
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-05 19:56:49 +0530

    Time: 455 ms (10.95%), Space: 74.4 MB (36.72%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        int i=0,x,n=chalk.size();
-        long long sum=0;
-        for(int v:chalk){
-            sum+=v;
+        k %= std::accumulate(cbegin(chalk), cend(chalk), 0LL);
+        for (auto i = 0; i < chalk.size(); ++i){
+            k -= chalk[i];
+            if (k < 0)
+                return i;
         }
-        int ks=k%sum;
-        // cout<<ks<<endl;
-        while(ks>0){
-            if(ks>=chalk[i]){
-              ks-=chalk[i]; 
-                i++;
-                // cout<<ks<<" ";
-            }
-            else{
-                x=i;
-                break;
-            }
-        }
-        return x%n;
+        return 0;
     }
 };
