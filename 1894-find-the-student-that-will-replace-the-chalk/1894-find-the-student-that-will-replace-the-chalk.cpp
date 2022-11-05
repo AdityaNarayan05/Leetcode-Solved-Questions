b2404f28cba1b6a0c11870081743e7d294cfed04
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-05 19:57:12 +0530

    Time: 180 ms (81.04%), Space: 74.5 MB (36.72%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,3 +1,11 @@
+static auto _______ = [](){
+    // turn off sync
+    std::ios::sync_with_stdio(false);
+    // untie in/out streams
+    std::cin.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
