b18f839bc5b515443288a92b704435624266d22d
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-08 19:56:31 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (82.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    string makeGood(string s,int sz=0) {
-        while (sz != s.size()) {
-            sz = s.size();
-            for (int i = 0; i + 1 < s.size(); ++i)
-                if (abs(s[i] - s[i + 1]) == 32)
-                    s = s.substr(0, i) + s.substr(i + 2);
-        }
-    
-        return s;
+    string makeGood(string s,int p=0) {
+        for (int i = 0; i < s.size(); ++i) {
+        if (p > 0 && abs(s[i] - s[p - 1]) == 32)
+            --p;
+        else
+            s[p++] = s[i];
+    }
+    return s.substr(0, p);
     }
 };
