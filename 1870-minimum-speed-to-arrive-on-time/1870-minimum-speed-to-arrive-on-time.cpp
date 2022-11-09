4195474f8dec82644e904947afd36d0189b4bf94
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-09 20:23:47 +0530

    Time: 275 ms (97.90%), Space: 101.6 MB (8.12%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -1,27 +1,21 @@
 class Solution {
-    bool canReachInTime(const vector<int>& dist, const double hour, int speed){
-        double time = 0;
-        for (int i = 0; i < dist.size() - 1; ++i)
-            time += ((dist[i] + speed - 1) / speed);
-        
-        time += ((double)dist.back()) / speed;
-        return time <= hour;
-    }
-    
 public:
-    int minSpeedOnTime(vector<int>& dist, double hour){
-        int N = dist.size();
-        if (hour <= (double)(N - 1))
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int l = 1, r = 1e9, n = dist.size();
+        if(n - 1 >= hour)
             return -1;
-        
-        int lo = 1, hi = 1e7, mi;
-        while (lo < hi){
-            mi = (lo + hi) / 2;
-            if (canReachInTime(dist, hour, mi))
-                hi = mi;
+        while(l < r){
+            int m = (l + r) / 2;
+            double sum = 0;
+            for(int i = 0; i < n - 1; ++i){
+                sum += ceil(dist[i] * 1.0 / m);
+            }
+            sum += dist[n - 1] * 1.0 / m;
+            if(sum > hour)
+                l = m + 1;
             else
-                lo = mi + 1;
+                r = m;
         }
-        return hi;
+        return l;
     }
 };
