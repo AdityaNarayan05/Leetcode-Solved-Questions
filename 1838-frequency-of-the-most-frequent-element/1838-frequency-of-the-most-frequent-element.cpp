ae5c0e2f22e60b7982698858f81f5280ef649b86
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-12 17:07:25 +0530

    Time: 301 ms (82.26%), Space: 99.1 MB (14.17%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        long long int left=0,right=0,n=nums.size(),dist=0,best=0;
+        //we need to think greedily for the solution
+        //uisng sliding window approach
+        int n=nums.size();
         sort(nums.begin(),nums.end());
-        while(right<n){
-            if(right>0 and nums[right]!=nums[right-1]){
-                dist+=(right-left)*(nums[right]-nums[right-1]);
+        long long l=0;
+        long long r=0;
+        long long total=0;
+        long long ans=INT_MIN;
+        //this is the main formula --nums[r]*size of sliding window<total till now +k
+        while(r<n){
+            total+=nums[r];
+            while(nums[r]*(r-l+1)>total+k){
+                total-=nums[l];
+                l++;
             }
-            right++;
-            while(dist>k){
-                dist-=(nums[right-1]-nums[left]);
-                left++;
-            }
-            best=max(best,right-left);
+            ans=max(ans,r-l+1);
+            r++;
+            
         }
-        return best;
+        return ans;
     }
 };
