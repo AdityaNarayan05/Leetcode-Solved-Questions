40f63b15f89971913966038957e20f4aadf3c888
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-12 16:45:59 +0530

    Time: 42 ms (48.25%), Space: 18.6 MB (87.66%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -4,10 +4,18 @@ public:
         int n = citations.size();
         if(!n)
             return 0;
-        for(int i=0;i<n;++i){
-             if(citations[i] >= n-i)
-                 return n-i;
+        int low =0,high=n-1;
+        int ans = 0;
+        while(low<=high){
+            int mid  = low + (high-low)/2;
+            if(citations[mid] >= n-mid){
+                ans = n-mid;
+                high = mid - 1;
+            }else{
+                low  = mid + 1;
+            }
         }
-        return 0;
+        
+        return ans;
     }
 };
