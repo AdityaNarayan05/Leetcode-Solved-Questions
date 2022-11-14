83859ef7a1a844b1d8e8526a92e5eaadf9d63766
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-14 21:29:25 +0530

    Time: 572 ms (19.89%), Space: 14 MB (95.67%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    unordered_map<int,int> mp;
-    int islands=0;
-    int removeStones(vector<vector<int>>& stones) {
-        for(int i=0;i<stones.size();++i)
-            funct(stones[i][0],~stones[i][1]);
-        
-        return stones.size()-islands;
+    int dfs(vector<vector<int>>&stones,int index,vector<bool>&visited,int&n){
+        visited[index]=true;
+        int result=0;
+        for(int i=0;i<n;i++)
+            if(!visited[i]&&(stones[i][0]==stones[index][0]||stones[i][1]==stones[index][1]))
+                result +=(dfs(stones,i,visited,n) + 1);
+        return result;
     }
-    void funct(int x,int y){
-        x=find(x);
-        y=find(y);
-        if(x!=y)
-            mp[x]=y,islands--;
-    }
-    int find(int x){
-        if(!mp.count(x))
-            mp[x]=x,islands++;
-        if(x!=mp[x])
-            mp[x]=find(mp[x]);
-        return mp[x];
+    int removeStones(vector<vector<int>>&stones) {
+        int n = stones.size();
+        vector<bool>visited(n,0);
+        int result=0;
+        for(int i=0;i<n;i++){
+            if(visited[i]){continue;}
+            result+=dfs(stones,i,visited,n);
+        }
+        return result;
     }
 };
