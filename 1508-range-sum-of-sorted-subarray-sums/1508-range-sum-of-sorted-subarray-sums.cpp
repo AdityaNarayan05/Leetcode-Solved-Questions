cd002d971a5babed0721e25827c76dc1ece16f12
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-17 18:40:06 +0530

    Time: 186 ms (56.50%), Space: 23.9 MB (33.54%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int> arr(n);
-        arr[0]=nums[0];
-        for(int i=1;i<nums.size();i++)
-            arr[i]=arr[i-1]+nums[i];
-        
-        vector<int> ans;
-        for(int i=0;i<n;i++)
-            ans.push_back(arr[i]);
-        
-        for(int i=0;i<n-1;i++)
-           for(int j=i+1;j<n;j++)
-                ans.push_back(arr[j]-arr[i]);
-        
-        sort(ans.begin(),ans.end());
-        long long sum=0;
-        int mod=1000000007;
-        for(int i=left-1;i<=right-1;i++)
-            sum+=ans[i];
-        
-        return sum%mod;
+        vector<int> sums { };
+        for (int i = 0; i < n; i++) {
+            int prefixSum = 0;
+            for (int j = i; j < n; j++) {
+                prefixSum += nums[j];
+                sums.push_back(prefixSum);
+            }
+        }
+        sort(begin(sums), end(sums));
+        int res = accumulate(sums.begin() + left - 1, sums.begin() + right, 0LL) % 1000000007;
+        return res;
     }
 };
