b689fe31f33d546eb343423f02bb49f574e5ded5
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-20 16:11:10 +0530

    Time: 277 ms (54.61%), Space: 53 MB (6.74%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target){
-        int n = nums.size();
         sort(nums.begin(), nums.end());
-    
-        int l = 0;
-        int r = n-1;
+        
         int ans = 0;
-        int mod = 1e9 + 7;
-    
-        vector<int> pow(n,1);
-    
-        for(int i=1; i<n; i++){
-            pow[i] = (pow[i-1] * 2) % mod;
+        int mod = 1e9+7;
+        
+        int i = 0;
+        int j = nums.size()-1;
+        
+        vector<int> pow;
+        pow.push_back(1);
+        for(int i = 1; i < nums.size(); i++) {
+            pow.push_back((pow.back() * 2)%mod);
         }
-        while(l <= r){
-            if(nums[l] + nums[r] > target){
-                r--;
+        
+        while(i <= j) {
+            if(nums[i] + nums[j] <= target) {
+                ans = (ans % mod + pow[j-i] % mod) % mod;
+                i++;
             }
-            else{
-                ans = (ans + pow[r - l]) % mod;
-                l++;
-            }
+            else j--;
         }
         return ans;
     }
