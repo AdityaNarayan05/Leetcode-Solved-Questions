807a8fde4c81897375564b08ab379defd70b857e
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-23 21:46:40 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.52%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.cpp
+++ 1201-ugly-number-iii/1201-ugly-number-iii.cpp
@@ -1,36 +1,25 @@
-typedef long long int ll;
-#define MAX_ANS 2e9
-
+typedef long long ll;
 class Solution {
 public:
-    ll gcd(ll a, ll b) {
-        if (a == 0) return b;
-        return gcd(b % a, a);
-    }
-    
-    ll lcm(ll a, ll b) {
-        return a * b / gcd(a, b);
-    }
-    
-    int count(ll num, ll a, ll b, ll c) {
-        return (int)(num / a + num / b + num / c
-            - num / lcm(a, b)
-            - num / lcm(b, c)
-            - num / lcm(a, c)
-            + num / (lcm(a, lcm(b, c))));
-    }
-    
-    int nthUglyNumber(int n, int a, int b, int c) {
-        int l=0,r=MAX_ANS,res=0;
-        while(l<=r){
-            int m=l+(r-l)/2;
-            if(count(m,a,b,c)>=n){
-                res=m;
-                r=m-1;
-            } else {
-                l=m+1;
-            }
+    int nthUglyNumber(int N, int A, int B, int C) {
+        ll a = A, b = B, c = C;
+        ll low = 1, high = 2*1e9;
+        ll ans = 1;
+        ll ab = (a*b/__gcd(a, b));
+        ll bc = (c*b/__gcd(c, b));
+        ll ac = (a*c/__gcd(c, a));
+        ll abc = (a*bc/__gcd(a, bc));
+        while(low<=high){
+            ll mid = low + (high - low)/2;
+            ll cnt = mid/a + mid/b + mid/c - (mid/ab) - (mid/bc ) - (mid/ac) + mid/abc;
+
+            if(cnt==N)
+                ans = mid;
+            if(cnt>=N)
+                high = mid-1;
+            else
+                low = mid+1;
         }
-        return res;
+        return ans;
     }
 };
