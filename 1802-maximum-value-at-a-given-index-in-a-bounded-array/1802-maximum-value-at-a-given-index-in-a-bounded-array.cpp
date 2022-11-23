64144e4b6e05afe43617b2ad1ea1370fc427cb43
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-23 21:55:42 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (75.06%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int maxValue(int n, int index, int maxSum) {
-        int maxV=1;
-        maxSum-=n;
-        int layer=1;
-        int points;
-        while(maxSum>0){
-            points=2*layer-1;
-            int r=index+layer-1-(n-1);
-            if(r>0)
-                points-=r;
-            
-            int l=layer-index-1;
-            if(l>0)
-                points-=l;
-            
-            if (points >= n || maxSum < points)
-                break;
-            
-            maxSum -= points;
-            layer++;
-            maxV++;
+        int right=n-index-1;
+        int left=index;
+        int low=1, high=maxSum, res=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long sum=0, leftsum=0, righsum=0, m=mid-1;
+            if(right<=m){
+                righsum=m*(m+1)/2 - (m-right)*(m-right+1)/2;
+            }else{
+                righsum=m*(m+1)/2+right-m;
+            }
+            if(left<=m){
+                leftsum=m*(m+1)/2 - (m-left)*(m-left+1)/2;
+            }else{
+                leftsum=m*(m+1)/2+left-m;
+            }
+            sum=mid+leftsum+righsum;
+            if(sum<=maxSum) 
+                res=mid, low=mid+1;
+            else high=mid-1; 
         }
-        return maxV+=maxSum/n;
+    return res;
     }
 };
