2f7cf8be28826d46dbc338306bbe2549e3cbfba9
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-23 20:26:48 +0530

    Time: 695 ms (13.73%), Space: 108.2 MB (36.84%) - LeetHub

--- 0911-online-election/0911-online-election.cpp
+++ 0911-online-election/0911-online-election.cpp
@@ -1,12 +1,10 @@
 class TopVotedCandidate {
 public:
-    unordered_map<int,int> mp;
-    vector<int> times;
+    map<int,int> mp;
     TopVotedCandidate(vector<int>& persons, vector<int>& times) {
         int n=persons.size();
         int lead=-1;
         unordered_map<int, int> count;
-        this->times=times;
         for(int i=0;i<n;i++){
             lead=++count[persons[i]]>=count[lead]?persons[i]:lead;
             mp[times[i]]=lead;
@@ -14,7 +12,7 @@ public:
     }
     
     int q(int t) {
-        return mp[*--upper_bound(times.begin(),times.end(),t)];
+        return (--mp.upper_bound(t))-> second;
     }
 };
 
