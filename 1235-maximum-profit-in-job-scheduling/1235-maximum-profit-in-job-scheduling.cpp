8026221492ed9e168a7777e410c78bc1407536fc
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-26 23:25:04 +0530

    Time: 187 ms (87.34%), Space: 59 MB (58.44%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -9,12 +9,32 @@ public:
         
         sort(arr.begin() , arr.end());
         dp[n] = 0;
-        for(int i = n-1 ; i >= 0 ; i--){
-            int idx = i+1;
-            while(idx < n && arr[idx][0] < arr[i][1])
-                idx++;
+        
+        // dp iterative approach
+//         for(int i = n-1 ; i >= 0 ; i--){
+//             int idx = i+1;
+//             while(idx < n && arr[idx][0] < arr[i][1])
+//                 idx++;
     
-            dp[i] = max(arr[i][2] + dp[idx] , dp[i+1]);   
+//             dp[i] = max(arr[i][2] + dp[idx] , dp[i+1]);   
+//         }
+        
+        // space optmised dp
+        for(int i = n-1 ; i >= 0 ; i--){
+            int key = arr[i][1];
+            int lo = i+1 , hi = n-1 , mid;
+            while(lo < hi){
+                mid = lo + (hi-lo)/2;
+                if(arr[mid][0] >= key)
+                    hi = mid;
+                else
+                    lo = mid+1;
+            }
+            int idx = lo;
+            if(lo == n || arr[lo][0] < arr[i][1])
+               idx = n;
+            
+            dp[i] = max(arr[i][2] + dp[idx] , dp[i+1]);
         }
         return dp[0];
     }
