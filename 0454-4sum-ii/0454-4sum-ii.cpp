0fe39a007b234cfe31df6f16f3bbccf9878f5319
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-27 19:56:30 +0530

    Time: 103 ms (99.38%), Space: 21.5 MB (99.22%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.cpp
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        int cnt=0;
-        unordered_map<int,int> mp;
-        for(auto &it1:nums1)
-            for(auto &it2:nums2)
-                mp[it1+it2]++;
+    void xsort(vector<int>& V){
+        sort(V.begin(), V.end());
+    }
+    int fourSumCount(vector<int>& nums1,vector<int>& nums2,vector<int>& nums3,vector<int>& nums4){
+        xsort(nums1); xsort(nums2); xsort(nums3); xsort(nums4);
+        int z=nums1.size()-1;
+        int  left=min(nums1[0]+nums2[0], -nums3[z]-nums4[z]);
+        int right=max(nums1[z]+nums2[z], -nums3[0]-nums4[0]);
+        vector<int> V(right-left+1, 0);
       
-        for(auto &it3:nums3)
-            for(auto &it4:nums4)
-                if(mp.count(0-it3-it4))
-                    cnt+=mp[0-it3-it4];
+        for(auto &a : nums1)
+            for(auto &b : nums2)
+                V[a+b-left]++;
+        
+        int cnt=0;
+        for(auto &a : nums3)
+            for(auto &b : nums4)
+                cnt+=V[-a-b-left];
         
         return cnt;
     }
