f338bfef245f08a1dbdaacf1097421f8a01609c9
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-28 19:28:56 +0530

    Time: 253 ms (59.45%), Space: 10.3 MB (30.64%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool> notPrime(n);
-        int cnt=0;
+        int ans =0;
+        vector<bool> temp(n,1);
+        
         for(int i=2;i<n;i++){
-            if(notPrime[i]==false){
-                cnt++;
-                for(int j=2;i*j<n;j++)
-                    notPrime[i*j]=true;
+            if(temp[i]){
+                ans++;
+                for(int j =2*i;j<n;j=j+i)
+                temp[j]=0;
             }
         }
-        return cnt;
+        
+        return ans;
     }
 };
