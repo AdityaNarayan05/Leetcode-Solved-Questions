6aa7451d4eec7e6598ead4f311d4d5423c7ca97f
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-11-30 15:13:09 +0530

    Time: 15 ms (76.45%), Space: 20.2 MB (93.17%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,37 +11,16 @@
  */
 class Solution {
 public:
-    // int diameterOfBinaryTree(TreeNode* root) {
-    //     int d=0;
-    //     diameter(root,d);
-    //     return d;
-    // }
-    // int diameter(TreeNode* root,int &d){
-    //     if(!root)
-    //         return 0;
-    //     int ld=diameter(root->left,d);
-    //     int rd=diameter(root->right,d);
-    //     d=max(d,ld+rd);
-    //     return  max(ld,rd)+1;
-    // }
-    
-    int maxDepth(TreeNode* root) {
-        if (!root) {
-            return 0;
-        }
-        
-        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    int de(TreeNode* root ,int &d)
+    {
+        if(root==NULL) return 0;
+         int l = de(root->left,d),r = de(root->right,d);
+         d = max(d,l+r);
+         return max(l,r)+1;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        if (!root) {
-            return 0;
-        }
-        
-        int leftDiam = diameterOfBinaryTree(root->left);
-        int rightDiam = diameterOfBinaryTree(root->right);
-        int rootDiam = maxDepth(root->left) + maxDepth(root->right);
-        
-        // Find max among these 3 elements
-        return max({leftDiam, rightDiam, rootDiam});
+        int d = 0;
+         de(root,d);
+        return d;   
     }
 };
