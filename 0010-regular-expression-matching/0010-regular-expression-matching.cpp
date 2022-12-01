1e0c97d18566c684a907d92666997e723833fa71
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-12-01 21:41:34 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (60.90%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        return isMatch(0,s,0,p);    
+        vector<vector<char>> dp(s.size()+1,vector<char>(p.size()+1,-1));
+        return isMatch(0,s,0,p,dp);    
     }
-    bool isMatch(int i, string& s, int j, string &p) {
+    bool isMatch(int i, string& s, int j, string &p, vector<vector<char>> &dp) {
+        if(dp[i][j] > -1) return dp[i][j];
         int pn=p.size(), sn = s.size();
-        if(j==pn)
-            return i==sn;
-        if(p[j+1]=='*'){
-            if(isMatch(i,s,j+2,p))
-                return 1;
-            while(i<sn && (p[j]==s[i]||p[j]=='.')) 
-				if(isMatch(++i,s,j+2,p)) 
-                    return 1;
-        } else if (i<sn && (p[j]=='.'|| s[i]==p[j]) && isMatch(i+1,s,j+1,p)) 
-			return 1;
-        
-        return 0;
+        if(j==pn) return dp[i][j] = i==sn;
+        if(p[j+1]=='*') {
+            if(isMatch(i,s,j+2,p,dp) || 
+               i<sn && (p[j] == '.' || s[i] == p[j]) && isMatch(i+1,s,j,p,dp)) 
+			   return dp[i][j] = 1;
+        } else if (i<sn && (p[j]=='.'|| s[i]==p[j]) && isMatch(i+1,s,j+1,p,dp)) 
+			return dp[i][j] = 1;
+        return dp[i][j] = 0;
     }
 };
