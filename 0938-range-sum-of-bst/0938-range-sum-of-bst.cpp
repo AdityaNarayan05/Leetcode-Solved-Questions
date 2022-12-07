454800bdc6d17119dd91dc5c02ae6ec502c2c5d6
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-12-07 20:37:56 +0530

    Time: 303 ms (39.88%), Space: 64.7 MB (18.63%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -11,47 +11,20 @@
  */
 class Solution {
 public:
-    vector<int> arr;
-    void inorder(TreeNode* root){
-        if(!root)
-            return;
-        inorder(root->left);
-        cout<<root->val<<" ";
-        arr.push_back(root->val);
-        inorder(root->right);
-    }
-    int bs(int num){
-        int l = 0, r = arr.size() - 1;
-        int mid;
-        
-        while (r - l > 1) {
-            int mid = (r + l) / 2;
-            if (arr[mid] < num) {
-                l = mid + 1;
-            } else {
-                r = mid;
-            }
-        } 
-        if (arr[l] == num) {
-        // cout << "Found"
-        //      << " At Index " << l << endl;
-            return l;
-        } else if (arr[r] == num) {
-        // cout << "Found"
-        //      << " At Index " << r << endl;
-            return r;
-        }
-        return 0;
-    }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        inorder(root);
-        int l=bs(low);
-        int r=bs(high);
-        int ans=0;
-        for(int i=l;i<=r;i++){
-            ans+=arr[i];
+        int result = 0;
+        if(root->val > low && root->left != NULL) {
+            result += rangeSumBST(root->left, low, high);
         }
-        return ans;
-        return 0;
+        
+        if(root->val < high && root->right != NULL) {
+            result += rangeSumBST(root->right, low, high);
+        }
+        
+        if(root->val <= high && root->val >= low) {
+            result += root->val;
+        }
+        
+        return result;
     }
 };
