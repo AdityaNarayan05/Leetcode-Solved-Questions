dc6d259c25cba585b961addbdf9d589dfee6b1f7
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-12-11 02:19:39 +0530

    Time: 946 ms (13.21%), Space: 221.8 MB (6.29%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum=accumulate(nums.begin(),nums.end(),0);//accumaulate add number of array;
-        // cout<<sum<<endl;
-        if(sum&1)
-            return false;
-        sum>>=1;//sum=sum>>1    or sum=sum/2;
-        vector<unordered_map<int,bool>> dp(nums.size());
-        return dfs(nums,sum,0,dp);
-    }
-    bool dfs(vector<int>&nums,int sum,int p,vector<unordered_map<int,bool>> &dp){
-        if(!sum)
-            return true;
-        if(sum<0 or p==nums.size())
-            return false;
-        auto it=dp[p].find(sum);
-        if(it!=dp[p].end())
-            return it->second;
-        return dp[p][sum]=dfs(nums,sum,p+1,dp) or dfs(nums,sum-nums[p],p+1,dp);
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int n = nums.size();
+        if(sum&1) 
+            return 0;
+        vector<unordered_set<int>> dp(n+1);
+        dp[0].insert(0);
+        for(int i=0;i<n;i++)
+            for(int s:dp[i]) {
+                if(s==sum/2)
+                    return 1;
+                if(s>sum/2) 
+                    continue;
+                dp[i+1].insert(s);
+                dp[i+1].insert(s+nums[i]);
+            }
+        return 0;
     }
 };
