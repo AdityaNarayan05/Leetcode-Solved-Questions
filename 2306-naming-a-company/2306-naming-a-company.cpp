78d8fe14ef584474b910ecc4eb459fc0628cf312
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-02-09 21:29:55 +0530

    Time: 315 ms (100.00%), Space: 79.9 MB (73.98%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
+    Solution() {
+        cin.tie(0);
+        ios::sync_with_stdio(0);
+    }
     long long distinctNames(vector<string>& ideas) {
-        long long disName = 0;
-        vector<unordered_set<string>> arr(26);
-        for (string s : ideas) 
-            arr[s[0] - 'a'].insert(s.substr(1));
-        
-        for (int i = 0; i < 25; i++) {
-            for (int j = i + 1; j < 26; j++) {
-                unordered_set<string> set;
-                set.insert(arr[i].begin(), arr[i].end());
-                set.insert(arr[j].begin(), arr[j].end());
-                disName += (arr[i].size() - set.size()) * (arr[j].size() - set.size());
+        unordered_map<string, int> headMask;
+        for (auto &idea : ideas) {
+            headMask[idea.substr(1)] |= 1 << (idea[0] - 'a');
+        }
+        array<array<array<int, 2>, 26>, 26> cnt{};
+        for (auto &[k, mask] : headMask) {
+            // cout << k << ' ' << mask << '\n';
+            for (int i = 0; i < 26; ++i) {
+                for (int j = i + 1; j < 26; ++j) {
+                    if (((mask >> i) & 1) ^ ((mask >> j) & 1)) {
+                        // cout << i << ' ' << j << ' ' << ((mask >> i) & 1) << ' ' << ((mask >> j) & 1) << '\n';
+                        ++cnt[i][j][(mask >> i) & 1];
+                    }
+                }
             }
         }
-        return disName * 2;
+        long long ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            for (int j = i + 1; j < 26; ++j) {
+                ans += 2ll * cnt[i][j][0] * cnt[i][j][1];
+            }
+        }
+        return ans;
     }
 };
