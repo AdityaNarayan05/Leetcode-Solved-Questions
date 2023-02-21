4f87bd2e02f599a4b5d64d0cd20a4ca6d81ede1b
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-02-21 13:32:18 +0530

    Time: 12 ms (34.86%), Space: 7 MB (48.69%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size()!=t.size()){
+            cout<<"1st one ";
+            return 0;
+        }
+            
+        
+        unordered_map<char,int> smp,tmp;
+        for(int i=0;i<s.size();i++){
+            if(smp[s[i]]!=tmp[t[i]])
+                return 0;
+            smp[s[i]]=i+1;
+            tmp[t[i]]=i+1;
+        }
+       
+        return 1;
+    }
+};
