cc2401c9549d8fde9c08cd3214bee3868578d178
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-03-24 17:07:33 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (9.90%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     vector<int> dp;
+    
     int rob(vector<int>& nums){
         dp.resize(nums.size()+1,-1);
         return robRec(nums,0);
     }
+    
     int robRec(vector<int> &nums,int idx){
         if(idx>=nums.size())
             return 0;
+        
         if(dp[idx]!=-1)
             return dp[idx];
+        
         return dp[idx]=max(nums[idx]+robRec(nums,idx+2),robRec(nums,idx+1));
     }
 };
