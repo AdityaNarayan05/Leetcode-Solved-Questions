a32888b69cecdfa7d19b92a4b51ae58ec89001f9
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-03-31 10:01:51 +0530

    Time: 24 ms (70.76%), Space: 13.7 MB (94.12%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& m){
+        if(m.empty())
+            return 0;
+        int n=m.size();
+        vector<bool> vis(n,false);
+        int g=0;
+        for(int i=0;i<n;i++){
+            g+= !vis[i] ? dfs(i,m,vis) , 1 : 0;
+        }
+        return g;
+    }
+ private:
+    void dfs(int i,vector<vector<int>>& m,vector<bool>& vis){
+        vis[i]=true;
+        for(int j=0;j<vis.size();j++){
+            if(i!=j&&m[i][j]&&!vis[j])
+                dfs(j,m,vis);
+        }
+    }
+};
