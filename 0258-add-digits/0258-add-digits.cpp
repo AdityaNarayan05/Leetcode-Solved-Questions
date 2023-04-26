d888b678b00cd55ba8e3b4da76f479ffb43743dd
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-04-26 14:42:59 +0530

    Time: 0 ms (100.00%), Space: 6 MB (7.98%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        long ans=0;
+        while(num){
+            ans+=num%10;
+            num/=10;
+        }
+        if(ans<10)
+            return ans;
+        
+        return addDigits(ans);
+    }
+};
