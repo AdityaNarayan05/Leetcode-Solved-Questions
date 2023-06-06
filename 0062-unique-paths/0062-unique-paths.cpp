d90685b6bc90ebc7fec42b480d344a7ab7097760
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-06-07 02:21:07 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (5.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int fun(int i,int j,vector<vector<int>> &dp){
+    vector<vector<int>> dp;
+    
+    int fun(int i,int j){
         if(i==0 && j==0)
             return 1;
         if(i<0 || j<0)
             return 0;
+        
         if(dp[i][j] != -1)
             return dp[i][j];
-        return dp[i][j] = fun(i-1,j,dp)+fun(i,j-1,dp);;
+        
+        return dp[i][j] = fun(i-1,j)+fun(i,j-1);
     }
     int uniquePaths(int m, int n) {   
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return fun(m-1,n-1,dp);
+        dp.resize(m,vector<int>(n,-1));
+        return fun(m-1,n-1);
     }
 };
