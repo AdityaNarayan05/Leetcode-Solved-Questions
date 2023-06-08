f4464579c1558e9e0ef2a93b7fb42a8bca679752
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-06-08 16:47:27 +0530

    Time: 79 ms (66.04%), Space: 13.1 MB (93.95%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size(); //size of the array
-        set<vector<int>> st;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                set<long long> hst;
-                for (int k = j + 1; k < n; k++) {
-                    
-                    long long sum = nums[i];
-                    sum+= nums[j];
-                    sum+= nums[k];
-                    long long fourth=target-sum;
+        int n=nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
         
-                    if (hst.find(fourth)!=hst.end()) {
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
+        for(int i=0;i<n;i++){
+            
+            if(i>0 and nums[i]==nums[i-1])
+                continue;
+            
+            for(int j=i+1;j<n;j++){
+                
+                if(j>i+1 and nums[j]==nums[j-1])
+                    continue;
+                
+                int lo=j+1,hi=n-1;
+                while(lo<hi){
+                    
+                    long long sum=nums[i]+nums[j];
+                    sum+=nums[lo];
+                    sum+=nums[hi];
+                    
+                    if(sum==target){
+                        vector<int> temp={nums[i],nums[j],nums[lo],nums[hi]};
+                        ans.push_back(temp);
+                        lo++;
+                        hi--;
+                        
+                        while(lo<hi and nums[lo]==nums[lo-1])
+                            lo++;
+                        while(lo<hi and nums[hi]==nums[hi+1])
+                            hi--;
                     }
-                    hst.insert(nums[k]);
+                    else if(sum<target)
+                        lo++;
+                    else
+                        hi--;
                 }
             }
         }
-        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
