c3f958906389f1e59a3c3bc5c21a7fb7e1ff6b09
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-06-14 16:40:50 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (97.37%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-        vector<int> v1, v2;
-        int i = 0, j = 0;
-        int n1 = version1.size(), n2 = version2.size();
-
-        while (i < n1 || j < n2) {
-            int num1 = 0, num2 = 0;
-
-            // Process version1
-            while (i < n1 && version1[i] != '.') {
-                num1 = num1 * 10 + (version1[i] - '0');
-                i++;
-            }
-
-            // Process version2
-            while (j < n2 && version2[j] != '.') {
-                num2 = num2 * 10 + (version2[j] - '0');
-                j++;
-            }
-
-            if (num1 < num2) return -1;
-            if (num1 > num2) return 1;
-
-            i++; // Move past the '.'
-            j++; // Move past the '.'
-
+    long long calculate(string& s, int& p) {
+        int n = s.length();
+        long long val = 0;
+        while (p < n && s[p] != '.') {
+            val = val * 10 + (s[p] - '0');
+            p++;
         }
+        if (p < n) {
+            p++; // Move past the '.'
+        }
+        return val;
+    }
 
+    int compareVersion(string version1, string version2) {
+        int n = version1.length();
+        int m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            long long v1 = (i < n) ? calculate(version1, i) : 0;
+            long long v2 = (j < m) ? calculate(version2, j) : 0;
+
+            if (v1 > v2) {
+                return 1;
+            } else if (v1 < v2) {
+                return -1;
+            }
+        }
         return 0;
     }
 };
