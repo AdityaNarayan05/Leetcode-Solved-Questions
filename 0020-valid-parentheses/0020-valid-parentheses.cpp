619918f218e4bfa1379732db319c2f376c3e6671
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-06-28 00:27:06 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (9.52%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto ch: s){
+            if(ch=='('||ch=='['||ch=='{')
+                st.push(ch);
+            else{
+                if(st.empty()||(st.top()=='('&&ch!=')')||(st.top()=='['&&ch!=']')||(st.top()=='{'&&ch!='}'))
+                    return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
