8fe164b9d467d5e6bb61e4239d8fbc2a2a3d6f05
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-07-04 16:31:07 +0530

    Time: 7 ms (66.23%), Space: 8.5 MB (98.36%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int funct(int i,int j,vector<vector<int>> &grid){
-        if(i==grid.size()-1)
-            return grid[i][j];
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        int down=grid[i][j]+funct(i+1,j,grid);
-        int diagonal=grid[i][j]+funct(i+1,j+1,grid);
-        
-        return dp[i][j]=min(down,diagonal);
-    }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        dp.resize(n,vector<int>(n,-1));
-        return funct(0,0,triangle);
+        vector<int> front(n,0), cur(n,0);
+    
+        for(int j=0;j<n;j++){
+            front[j] = triangle[n-1][j];
+        }
+
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+
+                int down = triangle[i][j]+front[j];
+                int diagonal = triangle[i][j]+front[j+1];
+
+                cur[j] = min(down, diagonal);
+            }
+            front=cur;
+        }
+
+        return front[0];
     }
 };
