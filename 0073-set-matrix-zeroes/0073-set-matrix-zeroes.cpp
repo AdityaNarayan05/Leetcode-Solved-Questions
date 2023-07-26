9eed5b7bfbe1970f363a9219128e88580b2832d4
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-07-26 20:51:52 +0530

    Time: 12 ms (92.01%), Space: 13.5 MB (16.49%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-       int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+    void setZeroes(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<int>row(n,0),col(m,0);
         
-        for (int i = 0; i < rows; i++) {
-            if (!matrix[i][0])
-                col0 = 0;
-            for (int j = 1; j < cols; j++)
-                if (!matrix[i][j])
-                matrix[i][0] = matrix[0][j] = 0;
-        }
-        for (int i = rows - 1; i >= 0; i--){
-            for (int j = cols - 1; j >= 1; j--)
-                if (!matrix[i][0] || !matrix[0][j])
-                    matrix[i][j] = 0;
-            if (!col0) 
-                matrix[i][0] = 0;
-        }
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(mat[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(row[i]==1||col[j]==1)
+                    mat[i][j]=0;
     }
 };
