4660bb560616dc81831feb3895f94a850fea551f
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-08-03 20:17:31 +0530

    Time: 7 ms (96.65%), Space: 10.7 MB (44.61%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
 	vector<vector<int>> combine(int n, int k) {
-		vector<vector<int>> result;
-		int i = 0;
-		vector<int> p(k, 0);
-		while (i >= 0) {
-			p[i]++;
-			if (p[i] > n) 
-                --i;
-			else if (i == k - 1) 
-                result.push_back(p);
-			else{
-			    ++i;
-			    p[i] = p[i - 1];
-			}
-		}
-		return result;
+		vector<int> setElements(n);
+        for (int i = 0; i < n; ++i) {
+            setElements[i] = i + 1; // Assuming the set contains elements 1 to n
+        }
+
+        vector<vector<int>> result;
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            if (__builtin_popcount(mask) == k) { // Check if the number of set bits in the mask is k
+                vector<int> combination;
+                for (int i = 0; i < n; ++i) {
+                    if (mask & (1 << i)) {
+                        combination.push_back(setElements[i]);
+                    }
+                }
+                result.push_back(combination);
+            }
+        }
+        return result;
 	}
 };
