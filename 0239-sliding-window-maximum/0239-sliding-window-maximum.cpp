af8d89942bc4a8106b76f3c51360bb0604010d3d
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-08-16 23:41:21 +0530

    Time: 237 ms (80.07%), Space: 148.8 MB (24.08%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,25 +1,5 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-    
-//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//         multiset<int,greater<int>> s;
-//         vector<int> ans;
-//         for(int i=0;i<k;i++)
-//             s.insert(nums[i]);
-        
-//         ans.push_back(*s.begin());
-//         for(int i=k;i<nums.size();i++){
-//             s.erase(s.lower_bound(nums[i-k]));
-//             s.insert(nums[i]);
-//             ans.push_back(*s.begin());
-//         }
-//         return ans;
-//     }
-    
-    // Heap solution 
-    
-    
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         priority_queue<pair<int,int>> pq;
         vector<int> ans;  
@@ -27,6 +7,7 @@ public:
         for(int i=0;i<nums.size();++i){
             while(!pq.empty() and pq.top().second<=(i-k))
                 pq.pop();
+            
             pq.push({nums[i],i}); 
             if(i>=k-1){
                 ans.push_back(pq.top().first);
@@ -35,26 +16,4 @@ public:
         }
         return ans; 
     }
-
-    
-    // Deque solution
-    
-    // vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    //     deque<pair<int,int>> dq;
-    //     vector<int> ans;
-    //     for(int i=0;i<nums.size();i++){
-    //         if(!dq.empty() and dq.front().second<=(i-k))
-    //             dq.pop_front();
-    //         while(!dq.empty() and dq.back().first<nums[i])
-    //             dq.pop_back();
-    //         dq.push_back({nums[i],i});
-    //         if(i>=k-1){
-    //             ans.push_back(dq.front().first);
-    //             // cout<<dq.front().first<<" ";
-    //         } 
-    //     }
-    //     return ans;
-    // }
-    
-    
 };
