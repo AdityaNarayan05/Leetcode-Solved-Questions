ecb1bc86ebbf1a8d461579fd7d4c9c1940151f7b
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-08-21 20:57:38 +0530

    Time: 18 ms (67.67%), Space: 13.5 MB (57.60%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        cout<<(s+s)<<endl;
-        cout<<(s+s).substr(1,s.size()*2-2)<<endl;
-        cout<<((s+s).substr(1,s.size()*2-2).find(s)!=-1) << endl;
+        // cout<<(s+s)<<endl;
+        // cout<<(s+s).substr(1,s.size()*2-2)<<endl;
+        // cout<<((s+s).substr(1,s.size()*2-2).find(s)!=-1) << endl;
         
         return (s+s).substr(1,s.size()*2-2).find(s)!=-1;
     }
