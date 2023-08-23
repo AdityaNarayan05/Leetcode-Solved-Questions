d54f50b86fb1993cd5e713e74ac3fa856569fb97
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2023-08-23 20:37:53 +0530

    Time: 4 ms (38.45%), Space: 6.4 MB (10.98%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        map<char,int> mp;
+        for(auto& ch : s){
+            mp[ch]+=1;
+        }
+        
+        priority_queue<pair<int,char>> pq;
+        string res="";
+        
+        for(auto &m:mp){
+            pq.push({m.second,m.first});
+        }
+        
+        while(pq.size()>1){
+            auto top1=pq.top();
+            pq.pop();
+            auto top2=pq.top();
+            pq.pop();
+            
+            res+=top1.second;
+            res+=top2.second;
+            
+            top1.first-=1;
+            top2.first-=1;
+            
+            if(top1.first>0)
+                pq.push(top1);
+            
+            if(top2.first>0)
+                pq.push(top2);
+        }
+        
+        if(!pq.empty()){
+            if(pq.top().first >1)
+                return "";
+            else
+                res+=pq.top().second;
+        }
+        
+        return res;
+    }
+};
